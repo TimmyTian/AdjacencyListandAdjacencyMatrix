0049b96609b5cdb6494defa849b1195f03ea0932
Rashiv Romio Bhusal <ROMIO@Rashivs-MacBook-Pro.local>
2016-08-18 18:32:10 +1000

    AdjList added

--- src/AdjList.java
+++ src/AdjList.java
@@ -10,30 +10,60 @@ import java.util.*;
  */
 public class AdjList <T extends Object> implements FriendshipGraph<T>
 {   
+		
+	Map<T, MyLinkedList<T>> vertlabels = new HashMap<T, MyLinkedList<T>>();
 
-
-        
+//       MyLinkedList[] linkedlist = new MyLinkedList[];
     /**
 	 * Contructs empty graph.
 	 */
     public AdjList() {
-    	// Implement me!
+    	
+    	
     } // end of AdjList()
     
     
     public void addVertex(T vertLabel) {
-        // Implement me!
+    	
+    	if(vertLabel==null) 
+    	{
+    		return;
+    	}
+    	if(vertlabels.containsKey(vertLabel)) {
+    		System.out.println("Already exists");
+    		return;
+    	}
+    	vertlabels.put(vertLabel, new MyLinkedList<T>(vertLabel));
+    	vertlabels.get(vertLabel).print();
+    	// Implement me!
     } // end of addVertex()
 	
     
     public void addEdge(T srcLabel, T tarLabel) {
-        // Implement me!
+        if(!(vertlabels.containsKey(srcLabel)&& vertlabels.containsKey(tarLabel)))
+        {
+        	System.out.println("edge doesnot exist");
+        	return;
+        }
+       
+
+        //if srclabel and tarlabel exist then throw exception
+       // otherwise add edge between them
+        	MyLinkedList<T> tmp = vertlabels.put(srcLabel, new MyLinkedList<T>(srcLabel));
+        	//vertlabels.put(tarLabel, new MyLinkedList<T>(tarLabel));
+    	// Implement me!
     } // end of addEdge()
 	
 
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
-        
+        if(vertlabels.containsKey(vertLabel)){
+        	
+        	for(int i=0;i<vertlabels.get(vertLabel).mLength;i++){
+        		neighbours.add(vertlabels.get(vertLabel).get(i));
+        	}
+        	
+        }
         // Implement me!
         
         return neighbours;
@@ -41,17 +71,27 @@ public class AdjList <T extends Object> implements FriendshipGraph<T>
     
     
     public void removeVertex(T vertLabel) {
-        // Implement me!
+       if(vertlabels.containsKey(vertLabel))
+       {
+    	   vertlabels.remove(vertLabel);
+       }
+    	// Implement me!
     } // end of removeVertex()
 	
     
     public void removeEdge(T srcLabel, T tarLabel) {
-        // Implement me!
+        if(vertlabels.containsKey(srcLabel)&& vertlabels.get(srcLabel).search(tarLabel))
+        {
+        	vertlabels.get(srcLabel).remove(tarLabel);
+        }
+    	// Implement me!
     } // end of removeEdges()
 	
     
     public void printVertices(PrintWriter os) {
-        // Implement me!
+        
+    	
+    	// Implement me!
     } // end of printVertices()
 	
     
