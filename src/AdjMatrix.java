b9b2ca081d32eb1d717370b0019328abbe4cd257
Rashiv Romio Bhusal <ROMIO@192-168-1-4.tpgi.com.au>
2016-08-21 19:43:54 +1000

    adjacency list fully working with shortest path and now added adjacency matrix

--- src/AdjMatrix.java
+++ src/AdjMatrix.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 
 /**
@@ -11,93 +12,112 @@ import java.util.*;
  */
 public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 {
-	private Map<T, Node<T>> Node;
-	Node[][] adjmatrix;
+	private Map<T, Integer> nodeMap;
+	private static final int GRAPH_SIZE = 10;
+	private static int counter = 0;
+	int[][] adjmatrix;
 	/**
 	 * Contructs empty graph.
 	 */
-    public AdjMatrix() {
-    	adjmatrix = new Node[Node.size()][Node.size()];
-    	// Implement me!
-    } // end of AdjMatrix()
-    
-    
-    public void addVertex(T vertLabel) {
-    	if(!Node.containsKey(vertLabel)){
-        // Node.put(vertLabel,);                  //TO DO  check here
-         //put((vertLabel, new Node[Node.size()][Node.size()]);
-        	}
-    
-        // Implement me!
-    } // end of addVertex()
-	
-    
-    public void addEdge(T srcLabel, T tarLabel) {
-    	Node distance;
-    	if(srcLabel != tarLabel){
-	        if(Node.containsKey(srcLabel) && !Node.get(srcLabel).search(tarLabel)){
-	        	Node.get(srcLabel).add(tarLabel);
-	        }
-	        if(Node.containsKey(tarLabel) && !Node.get(tarLabel).search(srcLabel)){
-	        	Node.get(tarLabel).add(srcLabel);
-	        }
-	       // Node[srcLabel][tarLabel]=distance;
-	       // Node[tarLabel][srcLabel]=distance;
-	    			}
-    	// Implement me!
-    } // end of addEdge()
-	
-    
-    public String toString() {
-    	int numberOfVertices = adjmatrix.length;
-    	String result = "";
+	public AdjMatrix() {
+		nodeMap = new HashMap<>();
+		adjmatrix = new int[GRAPH_SIZE][GRAPH_SIZE];  //TO DO check in lab for size of an array
+		for (int i = 0; i < adjmatrix.length; i++) {
+			for (int j = 0; j < adjmatrix[i].length; j++) {
+				adjmatrix[i][j] = 0;
+			}
+		}
+	} // end of AdjMatrix()
 
-    	// For Adjacency Matrix
-    	for (int j = 0, k = 0; j < numberOfVertices; j++) {
-    	    result += j + ":";
-    	    for (k = j; k < numberOfVertices; k++) {
-    		if (adjmatrix[j][k] != null) {
-    		    result += " (" + k + "," + adjmatrix[j][k] + ")";
+
+	public void addVertex(T vertLabel) {
+		if(!nodeMap.containsKey(vertLabel)){
+			nodeMap.put(vertLabel,counter);
+			counter++;
+		}
+
+	} // end of addVertex()
+
+
+	public void addEdge(T srcLabel, T tarLabel) {
+		int srcInt = nodeMap.get(srcLabel);
+		int tarInt = nodeMap.get(tarLabel);
+		adjmatrix[srcInt][tarInt] = 1;
+		adjmatrix[tarInt][srcInt] = 1;
+		
+//		Node distance;
+//		if(srcLabel != tarLabel){
+//			if(nodeMap.containsKey(srcLabel) && !nodeMap.get(srcLabel).search(tarLabel)){
+//				nodeMap.get(srcLabel).add(tarLabel);
+//			}
+//			if(nodeMap.containsKey(tarLabel) && !nodeMap.get(tarLabel).search(srcLabel)){
+//				nodeMap.get(tarLabel).add(srcLabel);
+//			}
+			// Node[srcLabel][tarLabel]=distance;
+			// Node[tarLabel][srcLabel]=distance;
+		//}
+		// Implement me!
+	} // end of addEdge()
+
+
+//	public String toString() {
+//		int numberOfVertices = adjmatrix.length;
+//		String result = "";
+//
+//		// For Adjacency Matrix
+//		for (int j = 0, k = 0; j < numberOfVertices; j++) {
+//			result += j + ":";
+//			for (k = j; k < numberOfVertices; k++) {
+//				if (adjmatrix[j][k] != null) {
+//					result += " (" + k + "," + adjmatrix[j][k] + ")";
+//				}
+//			}
+//		}
+//		return toString();
+//	}
+
+	public ArrayList<T> neighbours(T vertLabel) {
+		ArrayList<T> neighbours = new ArrayList<T>();
+
+		// Implement me!
+
+		return neighbours;
+	} // end of neighbours()
+
+
+	public void removeVertex(T vertLabel) {
+		// Implement me!
+	} // end of removeVertex()
+
+
+	public void removeEdge(T srcLabel, T tarLabel) {
+		// Implement me!
+	} // end of removeEdges()
+
+
+	public void printVertices(PrintWriter os) {
+		String abc = null;
+    	for(Entry<T, Integer> e: nodeMap.entrySet()) {
+    		if(abc==null){
+    			abc = ""+e.getKey();
+    		}else{
+    			abc = abc + " "+ e.getKey();
     		}
-    	    }
-    	    }
-		return toString();
-    }
+    	}
+    	os.println(abc);
+	} // end of printVertices()
 
-    public ArrayList<T> neighbours(T vertLabel) {
-        ArrayList<T> neighbours = new ArrayList<T>();
-        
-        // Implement me!
-        
-        return neighbours;
-    } // end of neighbours()
-    
-    
-    public void removeVertex(T vertLabel) {
-        // Implement me!
-    } // end of removeVertex()
-	
-    
-    public void removeEdge(T srcLabel, T tarLabel) {
-        // Implement me!
-    } // end of removeEdges()
-	
-    
-    public void printVertices(PrintWriter os) {
-        // Implement me!
-    } // end of printVertices()
-	
-    
-    public void printEdges(PrintWriter os) {
-        // Implement me!
-    } // end of printEdges()
-    
-    
-    public int shortestPathDistance(T vertLabel1, T vertLabel2) {
-    	// Implement me!
-    	
-        // if we reach this point, source and target are disconnected
-        return disconnectedDist;    	
-    } // end of shortestPathDistance()
-    
+
+	public void printEdges(PrintWriter os) {
+		// Implement me!
+	} // end of printEdges()
+
+
+	public int shortestPathDistance(T vertLabel1, T vertLabel2) {
+		// Implement me!
+
+		// if we reach this point, source and target are disconnected
+		return disconnectedDist;    	
+	} // end of shortestPathDistance()
+
 } // end of class AdjMatrix
